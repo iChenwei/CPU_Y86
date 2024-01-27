b57b392a8595a7786ff388fac5cd172ab05b33f0
Will‘s Mac mini <295429932@qq.com>
2024-01-27 19:32:58 +0800

    update fetch_stage/define.v

--- single_cycle/fetch_stage/fetch.v
+++ single_cycle/fetch_stage/fetch.v
@@ -1,24 +1,4 @@
 `include "define.v"
-// icode
-`define    IHALT        4'H0
-`define    INOP         4'H1
-`define    ICMOVQ       4'H2
-`define    IIRMOVQ      4'H3
-`define    IRMMOVQ      4'H4
-`define    IMRMOVQ      4'H5
-`define    IOPQ         4'H6
-`define    IJXX         4'H7
-`define    ICALL        4'H8
-`define    IRET         4'H9
-`define    IPUSHQ       4'HA
-`define    IPOPQ        4'HB
-
-// ifunc
-`define    FADDL        4'H0
-`define    FSUBL        4'H1
-`define    FANDL        4'H2
-`define    FXORL        4'H3
-
 // module
 module fetch (
     // input signal 
